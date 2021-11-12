42161a9b326bba9de679dfb8fac8609e5dcde6be
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-11-12 22:55:49 +0100

    add models and migrations

---
+++ database/migrations/2021_11_12_213706_create_sliders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSlidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sliders', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('slug')->unique();
+            $table->text('content')->nullable();
+            $table->string('image');
+            $table->string('button_text')->nullable();
+            $table->string('button_url')->nullable();
+            $table->boolean('active')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sliders');
+    }
+}
