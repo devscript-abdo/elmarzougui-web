c664cb001b1d75c1bdf050625e8faecbdd122ed7
Elmarzougui Abdelghafour <elmarzouguiabdelghafour@gmail.com>
2021-11-13 00:54:01 +0100

    configure Contact us validation

--- app/Http/Controllers/Site/ContactController.php
+++ app/Http/Controllers/Site/ContactController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Site;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Contact\ContactRequest;
+use App\Mail\Contact\ContactUsMail;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
@@ -12,5 +14,20 @@ class ContactController extends Controller
     public function index()
     {
         return view('theme.contact.index');
+    }
+
+    public function store(ContactRequest $request)
+    {
+        $data = $request->only(['name','email','phone','content']);
+
+        dd($data);
+        $email = setting('contact.email_reciver') ?? 'contact@' . request()->getHost();
+
+        if ($email) {
+
+            Mail::to($email)->send(new ContactUsMail($data));
+
+            return redirect()->route('contact')->with('isSent', 'merci pour votre message');
+        }
     }
 }
